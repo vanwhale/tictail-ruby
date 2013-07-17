ccc2bfa48c458b905dfb65ac997c32ff46f0dda6
Evan Whalen <evanwhalen@gmail.com>
2013-07-17 18:22:32 +0200

    add store id to access token

--- lib/tictail/access_token.rb
+++ lib/tictail/access_token.rb
@@ -2,11 +2,12 @@ module Tictail
 
   class AccessToken
     
-    attr_accessor :token, :store
+    attr_accessor :token, :store, :store_id
     
     def initialize(attributes)
-      self.token = attributes[:token]
-      self.store = attributes[:store]
+      @token = attributes[:token]
+      @store = attributes[:store]
+      @store_id = attributes[:store_id] || store['id']
     end
   end
 end
